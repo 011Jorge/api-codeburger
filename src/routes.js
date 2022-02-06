03cee34c2d88ea4b069b7a10adaa320853677645
011Jorge <vicentejorge187@gmail.com>
2022-02-05 21:40:44 -0300

    create table categories

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from "./config/multer";
 import ProductController from "./app/controllers/ProductController";
 import SessionController from "./app/controllers/SessionController";
 import UserController from "./app/controllers/UserController";
+import CategoryController from "./app/controllers/CategoryController";
 
 import authMiddlewares from "./app/middlewares/auth";
 
@@ -18,5 +19,8 @@ routes.use(authMiddlewares); // será chamado por todas as rotas abaixo
 
 routes.post("/products", upload.single("file"), ProductController.store);
 routes.get("/products", ProductController.index);
+
+routes.post("/categories", CategoryController.store);
+routes.get("/categories", CategoryController.index);
 
 export default routes;
