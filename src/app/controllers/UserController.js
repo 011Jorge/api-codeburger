263950fee7ce823cfb29f560b63f286da278062f
011Jorge <vicentejorge187@gmail.com>
2023-02-23 12:02:54 -0300

    :zap: perf: creating password hashes

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,17 +8,17 @@ class UserController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
-      password_hash: Yup.string().required().min(6),
+      password: Yup.string().required().min(6),
       admin: Yup.boolean(),
     })
 
     try {
-      await schema.validateSync(req.body, { abortEarly: false })
+      schema.validateSync(req.body, { abortEarly: false })
     } catch (err) {
       return res.status(400).json({ error: err.errors })
     }
 
-    const { name, email, password_hash, admin } = req.body
+    const { name, email, password, admin } = req.body
 
     const userExists = await User.findOne({
       where: { email },
@@ -32,7 +32,7 @@ class UserController {
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     })
 
