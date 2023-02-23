ceb401f9ac9805ed5ca902853b001a9b5372a55a
011Jorge <vicentejorge187@gmail.com>
2023-02-23 11:10:59 -0300

    :bricks: ci: created userControllers

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 import { Router } from 'express'
-import { v4 } from 'uuid'
 
-import User from './app/models/User'
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'Jorge',
-    email: 'rodolfos@email.com',
-    password_hash: '12345',
-    admin: true,
-  })
-
-  return res.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
