ef2abff97d77337848a31216650059147bd3d09d
011Jorge <vicentejorge187@gmail.com>
2023-02-23 11:42:51 -0300

    :zap: perf: validating data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,23 @@
 import { v4 } from 'uuid'
+import * as Yup from 'yup'
 
 import User from '../models/User'
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    })
+
+    try {
+      await schema.validateSync(req.body, { abortEarly: false })
+    } catch (err) {
+      return res.status(400).json({ error: err.errors })
+    }
+
     const { name, email, password_hash, admin } = req.body
 
     const user = await User.create({
